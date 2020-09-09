e14a551edc9de52be78d55434557783e0c661f86
aarti1207 <aarti1207@gmail.com>
2020-09-10 00:25:23 +0530

    Create data.sql

---
+++ TodoList/todos/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, name,description,target_date,is_completed)
+values(10001, 'Arti', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, name ,description,target_date,is_completed)
+values(10002, 'Arti', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, name,description,target_date,is_completed)
+values(10003, 'Sujji', 'Learn Microservices', sysdate(), false);
